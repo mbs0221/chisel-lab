dcf7f9743203d29e6d6486bfd5df2daf23cccc27
mbs0221 <mbs2021@163.com>
2021-05-28 16:18:24 +0800

    lab6 passed!

--- lab6/src/main/scala/CountSevenSeg.scala
+++ lab6/src/main/scala/CountSevenSeg.scala
@@ -10,7 +10,19 @@ class CountSevenSeg extends Module {
   val sevSeg = WireInit("b1111111".U(7.W))
 
   // *** your code starts here
-
+  // counter
+  val num = RegInit(0.U(4.W))
+  num := num + 1.U
+  // divider
+  val counter = RegInit(500000.U)
+  counter := counter - 1.U
+  when (counter === 0.U) {
+    counter := 500000.U
+  }
+  // decoder
+  val decoder = Module(new SevenSegDec())
+  decoder.io.in := num
+  io.seg := decoder.io.out
 
   // *** your code ends here
 
