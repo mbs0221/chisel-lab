8ef696bae3abf8dabffae97440ff2bdc4782713d
mbs0221 <mbs2021@163.com>
2021-05-28 15:14:34 +0800

    passed lab4!

--- lab4/src/main/scala/SevenSegDecoder.scala
+++ lab4/src/main/scala/SevenSegDecoder.scala
@@ -1,5 +1,6 @@
 import chisel3._
 import chisel3.util._
+import scala.annotation.switch
 
 class SevenSegDecoder extends Module {
   val io = IO(new Bundle {
@@ -11,7 +12,18 @@ class SevenSegDecoder extends Module {
   val sevSeg = WireInit(0.U)
 
   // ***** your code starts here *****
-
+  switch (io.sw) {
+    is(0.U) { io.seg := "b1111110".U }
+    is(1.U) { io.seg := "b0110000".U }
+    is(2.U) { io.seg := "b1101101".U }
+    is(3.U) { io.seg := "b1111001".U }
+    is(4.U) { io.seg := "b0110011".U }
+    is(5.U) { io.seg := "b1011011".U }
+    is(6.U) { io.seg := "b1011111".U }
+    is(7.U) { io.seg := "b1110000".U }
+    is(8.U) { io.seg := "b1111111".U }
+    is(9.U) { io.seg := "b1111011".U }    
+  }
   // ***** your code ends here *****
 
   io.seg := ~sevSeg
