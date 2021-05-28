9dd936328911009c5b227a880ab694411acde760
mbs0221 <mbs2021@163.com>
2021-05-28 15:46:45 +0800

    lab4 passed

--- lab4/src/main/scala/SevenSegDecoder.scala
+++ lab4/src/main/scala/SevenSegDecoder.scala
@@ -13,16 +13,22 @@ class SevenSegDecoder extends Module {
 
   // ***** your code starts here *****
   switch (io.sw) {
-    is(0.U) { io.seg := "b1111110".U }
-    is(1.U) { io.seg := "b0110000".U }
-    is(2.U) { io.seg := "b1101101".U }
-    is(3.U) { io.seg := "b1111001".U }
-    is(4.U) { io.seg := "b0110011".U }
-    is(5.U) { io.seg := "b1011011".U }
-    is(6.U) { io.seg := "b1011111".U }
-    is(7.U) { io.seg := "b1110000".U }
-    is(8.U) { io.seg := "b1111111".U }
-    is(9.U) { io.seg := "b1111011".U }    
+    is(0.U) { sevSeg := "b0111111".U }
+    is(1.U) { sevSeg := "b0000110".U }
+    is(2.U) { sevSeg := "b1011011".U }
+    is(3.U) { sevSeg := "b1001111".U }
+    is(4.U) { sevSeg := "b1100110".U }
+    is(5.U) { sevSeg := "b1101101".U }
+    is(6.U) { sevSeg := "b1111101".U }
+    is(7.U) { sevSeg := "b0000111".U }
+    is(8.U) { sevSeg := "b1111111".U }
+    is(9.U) { sevSeg := "b1101111".U }    
+    is(10.U) { sevSeg := "b1110111".U }
+    is(11.U) { sevSeg := "b1111100".U }
+    is(12.U) { sevSeg := "b1011000".U }
+    is(13.U) { sevSeg := "b1011110".U }
+    is(14.U) { sevSeg := "b1111001".U }
+    is(15.U) { sevSeg := "b1110001".U }
   }
   // ***** your code ends here *****
 
